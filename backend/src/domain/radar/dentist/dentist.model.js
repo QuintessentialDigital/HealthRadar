ad5f7c5ff9bc59c4db4b0e7fea68bff20452a3c0
QuintessentialDigital <88312450+QuintessentialDigital@users.noreply.github.com>
2025-12-01 11:23:08 +0000

    Update dentist.model.js

--- backend/src/domain/radar/dentist/dentist.model.js
+++ backend/src/domain/radar/dentist/dentist.model.js
@@ -1,1 +1,11 @@
+const mongoose = require("mongoose");
+
+const DentistPractice = new mongoose.Schema({
+  name: String,
+  postcode: String,
+  phone: String,
+  acceptingNew: Boolean
+}, { timestamps: true });
+
+module.exports = mongoose.model("DentistPractice", DentistPractice);
 
