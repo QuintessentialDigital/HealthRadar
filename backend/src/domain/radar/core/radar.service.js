8064724b4d3b1dfa6a641b9054de01fd8695dd9e
QuintessentialDigital <88312450+QuintessentialDigital@users.noreply.github.com>
2025-12-01 11:21:54 +0000

    Add preview function to radar service

--- backend/src/domain/radar/core/radar.service.js
+++ backend/src/domain/radar/core/radar.service.js
@@ -1,1 +1,9 @@
+const { getRadar } = require("./radar.registry");
+
+async function preview(radarType, params) {
+  const radar = getRadar(radarType);
+  return radar.preview(params);
+}
+
+module.exports = { preview };
 
