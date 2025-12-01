920aa7fd4dc5bb485f24e51825f2e315119eb61e
QuintessentialDigital <88312450+QuintessentialDigital@users.noreply.github.com>
2025-12-01 11:24:15 +0000

    Add route to list radars in index.routes.js

--- backend/src/interfaces/http/routes/index.routes.js
+++ backend/src/interfaces/http/routes/index.routes.js
@@ -1,1 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const { listRadars } = require("../../domain/radar/core/radar.registry");
+
+router.get("/", (req, res) => {
+  res.json({ ok: true, radars: listRadars() });
+});
+
+module.exports = router;
 
