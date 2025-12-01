a56e3ef289b3f3438eafef5d6321923f18f49313
QuintessentialDigital <88312450+QuintessentialDigital@users.noreply.github.com>
2025-12-01 11:19:22 +0000

    Add MongoDB connection function using Mongoose

--- backend/src/core/db/mongoose.js
+++ backend/src/core/db/mongoose.js
@@ -1,1 +1,14 @@
+const mongoose = require("mongoose");
+const { mongoUri } = require("../../config/env");
+
+async function connectDB() {
+  if (!mongoUri) {
+    throw new Error("❌ MONGODB_URI is not set in environment variables");
+  }
+
+  await mongoose.connect(mongoUri, { autoIndex: true });
+  console.log("🚀 Connected to MongoDB");
+}
+
+module.exports = connectDB;
 
