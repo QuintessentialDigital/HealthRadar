da3a28acbfb2cffed1941b7580f3e1bf4e200e72
QuintessentialDigital <88312450+QuintessentialDigital@users.noreply.github.com>
2025-12-04 22:28:06 +0000

    Delete backend/src/domain directory

--- backend/src/domain/radar/core/radar.service.js
+++
@@ -1,9 +0,0 @@
-const { getRadar } = require("./radar.registry");
-
-async function preview(radarType, params) {
-  const radar = getRadar(radarType);
-  return radar.preview(params);
-}
-
-module.exports = { preview };
-
